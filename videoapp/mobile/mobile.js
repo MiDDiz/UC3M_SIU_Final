19447b5b410917eb4f74dc586663793f05ccb4e1
MiDDiz <xxmidone30xx@gmail.com>
2023-03-31 10:36:54 +0200

    first approx

--- videoapp/mobile/mobile.js
+++ videoapp/mobile/mobile.js
@@ -1,16 +1,40 @@
-var possY_inicial;
-var posY_final;
-var tiempo_i;
-var tiempo_f;
+var possY_inicial = 0;
+var posY_final = 0;
+var tiempo_i = 0;
+var tiempo_f = 0;
 
 
 
+// Codigo incompleto para adaptarlo 
 
 const acl = new Accelerometer({ frequency: 60 });
-acl.addEventListener("reading", () => {
-  acl.y
-  console.log(`Acceleration along the Z-axis ${acl.z}`);
+/*
+acl.addEventListener("reading", () => { // Cambiar evento por touch
+  possY_inicial = acl.y; 
 });
+*/
 
 acl.start();
+/*
+setInterval( () => {
+  // Meter cambios
+  let diff_ejeY = Math.abs(posY_final - possY_inicial);
+  // Luego actulizar 
+  posY_final =  possY_inicial;
+
+})
+*/
+
+acl.addEventListener("touchstart", (evento) => { //
+  possY_inicial = evento.changedTouches[0].pageX;
+  tiempo_i= new Date.now()
+});
+acl.addEventListener("move", (evento) =>  {
+let diff_ejeY = Math.abs(posY_final - possY_inicial);
+tiempo_f= new Date.now()
+let velocidad = diff_ejeY / (tiempo_f - tiempo_i)
+if((velocidad) > 50){
+  // Si se cumple el gesto hacer .....
+}
+});
 
