8bf1e4e6cd0213c68e9d6f12cd123c4e289bd2d0
MiDDiz <xxmidone30xx@gmail.com>
2023-04-18 16:12:30 +0200

    merged player js html css

--- videoapp/player/player.js
+++ videoapp/player/player.js
@@ -1,5 +1,4 @@
 
-
 /* Bootstrap snippets */
 
 // Video autoplayer
@@ -7,35 +6,41 @@ const clip = document.querySelectorAll(".hover-to-play");
 for (let i = 0; i < clip.length; i++) { clip[i].addEventListener("mouseenter", function (e) { clip[i].play();
   }); clip[i].addEventListener("mouseout", function (e) { clip[i].pause(); }); }
 
+ /* Funcion para cambiar el video del modal */
+function playVideo(videoSource, type) {
+	var player = videojs(document.querySelector('.video-js'));
+	player.src({
+		src: videoSource,
+		type: type
+	});
+	player.play();
+}
 
+function pausePlayer(){
+	var player = videojs(document.querySelector('.video-js'));
+	player.pause();
+	
+}
+
+	/* Función para lanzar el modal al clickar en un video */
   $(function() {
-	$("video").click(function (e) {
+	$(".video-wrapper video").click(function (e) {
 		e.preventDefault();
 		console.log("ay");
 		var theModal = $(this).data("target"),
 			videoSRC = $(this).attr("src"),
 			videoSRCauto = videoSRC + "";
 		console.log(videoSRCauto);
-		$(theModal + ' source').attr('src', videoSRCauto);
-		$(theModal + ' video').load(videoSRCauto);
-		$(theModal + ' button.close').click(function () {
-			$(theModal + ' source').attr('src', videoSRC);
+
+		playVideo(videoSRCauto, "video/webm");
+
+		/* When modal is hidden, pause video on background */
+		$(theModal).on('hidden.bs.modal', function () {
+			pausePlayer();
 		});
+		
+		
 		$(theModal).modal("toggle");
+
 	});
   });
-
-	/*
-	$(function() {
-		$(".video").click(function () {
-		var theModal = $(this).data("target"),
-			videoSRC = $(this).attr("data-video"),
-			videoSRCauto = videoSRC + "";
-		$(theModal + ' source').attr('src', videoSRCauto);
-		$(theModal + ' video').load();
-		$(theModal + ' button.close').click(function () {
-			$(theModal + ' source').attr('src', videoSRC);
-		});
-		});
-	});
-	*/
