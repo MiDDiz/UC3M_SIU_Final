c61f9301b50a4ed598b30880413ca70459d78dbf
MiDDiz <xxmidone30xx@gmail.com>
2023-04-18 18:36:41 +0200

    mando

--- videoapp/player/player.js
+++ videoapp/player/player.js
@@ -1,3 +1,115 @@
+/* Get all video sections */
+
+/* Return all video elements in a 2d array*/
+function getAllVideos(){
+	const allSections = document.querySelectorAll(".video-section");
+	let videoFrames = [];
+	allSections.forEach(videoSection => {
+		let newVideoRow = videoSection.querySelectorAll(".video-wrapper");
+		videoFrames.push(newVideoRow);
+	});
+	console.log(videoFrames);
+	return (videoFrames);
+}
+const videoFrames = getAllVideos();
+
+let userPlayer = {
+	coordsVideoSelected: { x:0, y: 0},
+}
+
+/* Make video pointed by coords selected and return it */
+function selectVideo(x, y, userPlayer) {
+	const videoSelected = videoFrames[y][x];
+	const prevVideoSelected = videoFrames[userPlayer.coordsVideoSelected.y][userPlayer.coordsVideoSelected.x];
+	/* Add selected CSS */
+	$(prevVideoSelected).removeClass("video-selected");
+	$(videoSelected).addClass("video-selected");
+
+	/* Change data in user object */
+	userPlayer.coordsVideoSelected.x = x;
+	userPlayer.coordsVideoSelected.y = y;
+	return (videoSelected);
+}
+/* Return true if coords is within borders */
+function checkVideoWithinBorders(x, y){
+	if (x < 0 || y < 0) return false;
+	if (y > videoFrames.length - 1) return false;
+	if (x > videoFrames[y].length - 1) return false;
+	return true;
+}
+
+
+/* BEGIN INTERFACE FUNCTIONS */
+
+
+function selectVideoRight(userPlayer){
+	if (checkVideoWithinBorders(
+		userPlayer.coordsVideoSelected.x + 1,
+		userPlayer.coordsVideoSelected.y
+	) == false){
+		return ;
+	}
+	return selectVideo(
+		userPlayer.coordsVideoSelected.x + 1, 
+		userPlayer.coordsVideoSelected.y,
+		userPlayer
+	);
+}
+
+function selectVideoLeft(userPlayer){
+	if (checkVideoWithinBorders(
+		userPlayer.coordsVideoSelected.x - 1,
+		userPlayer.coordsVideoSelected.y
+	) == false){
+		return ;
+	}
+	return selectVideo(
+		userPlayer.coordsVideoSelected.x - 1, 
+		userPlayer.coordsVideoSelected.y,
+		userPlayer
+	);
+}
+
+function selectVideoTop(userPlayer){
+	if (checkVideoWithinBorders(
+		userPlayer.coordsVideoSelected.x,
+		userPlayer.coordsVideoSelected.y - 1
+	) == false){
+		return ;
+	}
+	return selectVideo(
+		userPlayer.coordsVideoSelected.x, 
+		userPlayer.coordsVideoSelected.y - 1,
+		userPlayer
+	);
+}
+
+function selectVideoBott(userPlayer){
+	if (checkVideoWithinBorders(
+		userPlayer.coordsVideoSelected.x,
+		userPlayer.coordsVideoSelected.y + 1
+	) == false){
+		return ;
+	}
+	return selectVideo(
+		userPlayer.coordsVideoSelected.x, 
+		userPlayer.coordsVideoSelected.y + 1,
+		userPlayer
+	);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* Bootstrap snippets */
 
@@ -44,3 +156,24 @@ function pausePlayer(){
 
 	});
   });
+
+/* Function to hook select video to arrows*/
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+	switch (key) {
+		case "ArrowLeft":
+			selectVideoLeft(userPlayer);
+			break;
+		case "ArrowRight":
+			selectVideoRight(userPlayer);
+			break;
+		case "ArrowUp":
+			selectVideoTop(userPlayer);
+			break;
+		case "ArrowDown":
+			selectVideoBott(userPlayer);
+			break;
+		case
+	}
+});
